3a6cafef7b25d20736d0bd5fa1e1e98b729c8d03
lxf_jh <liuxf@jihualab.com>
2022-05-31 14:24:39 +0800

    Initial commit

---
+++ DecisionTrees.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# Project    :ExpertSystem
+# File       :DecisionTrees.py
+# Time       :2022/5/31 14:23
+# Author     :Felix
+# version    :python 3.x
+# Description：
+决策树们
+各类零件的决策树
+"""
+
